55b8fab779fbdf84b5c0168f0b9fd29d71ee09c4
Brent Ransom <sadumedusa@gmail.com>
2019-02-23 09:58:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,19 +2,17 @@ require_relative 'config/environment'
 require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
-  
-  get '/' do 
+
+  get '/' do
     erb :index
   end
-  
+
   get '/new' do
     erb :create_puppy
   end
-  
+
   post '/puppy' do
     @puppy = Puppy.new(params['name'], params['breed'], params['age'])
     
     erb :display_puppy
   end
-
-end
